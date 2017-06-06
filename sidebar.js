968595c3444fb66cfaf49c32607dfe91b5a06692
TadCordle <tcordle1@umbc.edu>
2017-06-06 10:59:13 -0400

    Minimize sidebar copy-paste

---
+++ sidebar.js
@@ -0,0 +1,9 @@
+document.write('\
+<table width=150>\
+<tr><td><h3>Links</h3></td></tr>\
+<tr><td><a href="https://tadcordle.github.io/index.html">Home</a></td></tr>\
+<tr><td><a href="https://tadcordle.github.io/projects/index.html">Projects</a></td></tr>\
+<tr><td><a href="https://tadcordle.github.io/blog/index.html">Blog</a></td></tr>\
+<tr><td><a href="https://tadcordle.github.io/contact.html">Contact</a></td></tr>\
+</table>\
+');

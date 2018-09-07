3f8e7e4cc7c5e404f872de558dcd1ef004341eb5
Tad Cordle <tad.cordle@gmail.com>
2018-09-06 23:19:55 -0400

    Update Sensorium page, remove music page

--- sidebar.js
+++ sidebar.js
@@ -3,7 +3,6 @@ document.write('\
 <tr><td><h3>Links</h3></td></tr>\
 <tr><td><a href="https://tadcordle.github.io/index.html">Home</a></td></tr>\
 <tr><td><a href="https://tadcordle.github.io/projects/index.html">Games</a></td></tr>\
-<tr><td><a href="https://tadcordle.github.io/music.html">Music</a></td></tr>\
 <tr><td><a href="https://tadcordle.github.io/blog/index.html">Blog</a></td></tr>\
 <tr><td><a href="https://tadcordle.github.io/contact.html">Contact</a></td></tr>\
 </table>\
